1eacbfa0f8ef17d00f8a5d80d140b82a558abc1e
MashoodAli-Official <MashoodAli3@gmail.com>
2024-09-18 20:58:43 +0400

    Project restructure

--- 01-Introduction/01-comment.js
+++
@@ -1,7 +0,0 @@
-// This is a single-line comment
-
-/* This is a 
-   multi-line comment */
-
-// The code below prints "Hello, World!" to the console
-console.log("Hello, World!");
---
+++ Ch.01-Introduction/03-Prompt/prompt.js
@@ -0,0 +1,3 @@
+var message = prompt("Please enter your name", "Iron Man")
+// here the Iron Man is a default value in prompt
+console.log(message);
