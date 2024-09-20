bdca3d7a1748cf8309a8dbb088f7aff9f72b1eb2
MashoodAli-Official <MashoodAli3@gmail.com>
2024-09-20 21:07:36 +0400

    comparison in javascript

---
+++ Ch.01-Introduction/07-Comparision/comparision.js
@@ -0,0 +1,29 @@
+let a = 10;
+let b = 20;
+
+// Equal ==
+console.log(a == b);
+
+// Equal: check both values and type of values.
+console.log(a === b);
+
+// Not equal !=
+console.log(a != b);
+
+// Greater than >
+console.log(a > b);
+
+// Smaller than <
+console.log(a < b);
+
+// Greater than or equal >=
+console.log(a >= b);
+
+// Greater than or equal <=
+console.log(a <= b);
+
+// Not equal: check both values and type of values.
+console.log(a !== b);
+
+// Smaller than: check both values and type of values.
+console.log(a !== b);
