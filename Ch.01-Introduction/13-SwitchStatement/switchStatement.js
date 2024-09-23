b74f6ef58006d0b067c8c8bac450da61a689f391
MashoodAli-Official <MashoodAli3@gmail.com>
2024-09-23 22:01:59 +0400

    switch statement chapter completed.

---
+++ Ch.01-Introduction/13-SwitchStatement/switchStatement.js
@@ -0,0 +1,15 @@
+let variable = 20;
+
+switch (variable){
+    case 10:
+        console.log("Value is 10");
+        break;
+    case 20:
+        console.log("value is 20");
+        break;
+    case 30:
+        console.log("value is 30");
+    default:
+        console.log("No value found");
+        break;    
+}
