1c86558615eb085d23b2e0ca58218999b14e9d6d
MashoodAli-Official <MashoodAli3@gmail.com>
2024-09-25 22:10:45 +0400

    while loop and do while loop chapter is done

---
+++ Ch.01-Introduction/16-WhileLoop/whileLoop.js
@@ -0,0 +1,5 @@
+var a = 0;
+while (a <= 100) {
+    console.log("Hello Universe : " + a);
+    a += 3;
+}
---
+++ Ch.01-Introduction/17-DoWhileLoop/doWhileLoop.js
@@ -0,0 +1,5 @@
+let i = 0;
+do {
+  console.log("hey there : " + i);
+  i++;
+} while (i < 5);
