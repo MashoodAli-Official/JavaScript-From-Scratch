34e60fc8c9200b147f6ea23dfc9cfc8cc54d91fa
MashoodAli-Official <MashoodAli3@gmail.com>
2024-09-25 21:20:46 +0400

    For loops and For in loop chapter is completed.

--- Ch.01-Introduction/15-ForInLoop/forInLoop.js
+++ Ch.01-Introduction/15-ForInLoop/forInLoop.js
@@ -1,4 +1,4 @@
-let myUniverse = {["Earth", "Venus", "Mars", "Jupiter", "Saturn", "Uranus", "Neptune", "Pluto"]}
-for (let i in myUniverse) {
-    console.log(i)
+var myUniverse = {a:"Pluto", b:"Venus", c:"Earth", d:"Mars", e:"Jupiter", f:"Saturn", g:"Uranus", h:"Neptune"}
+for (var item in myUniverse){
+    console.log(item,":", myUniverse[item])
 }
