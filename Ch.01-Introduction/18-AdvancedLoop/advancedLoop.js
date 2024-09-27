209da78d0e6af8c65e05771d4fb7aea7ef182248
MashoodAli-Official <MashoodAli3@gmail.com>
2024-09-27 16:53:20 +0400

    Advance loop

--- Ch.01-Introduction/18-AdvancedLoop/advancedLoop.js
+++ Ch.01-Introduction/18-AdvancedLoop/advancedLoop.js
@@ -1,12 +1,8 @@
 "use strict";
 
-for (var i = 0; i <= 100; i += 2)
-{
-	if (i === 50)
-	{
-		//break;
+for (var i = 0 ; i <= 66 ; i+=3){
+	if (i % 3 == 0){
 		continue;
 	}
-
-	console.log("This is position for I: " + i);
+	console.log("hi" + i)
 }
