fdbf6178db4502d9701282c768968cd134e2a055
MashoodAli-Official <MashoodAli3@gmail.com>
2024-10-08 21:03:51 +0500

    Math object

---
+++ Ch.02-ObjectsInJs/07-MathObject/mathObject.js
@@ -0,0 +1,6 @@
+"use Strict";
+
+console.log("Pi value: ",Math.PI); // 3.141592653589793
+console.log("Power of 2 and 3: ",Math.pow(2,3));
+console.log("Square root of 100: ", Math.sqrt(100)); //10
+console.log("Round of 100.05: ", Math.round(100.05)); //100
