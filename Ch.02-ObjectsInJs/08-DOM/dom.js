9e5bf4e1eec2509b27b62deb209bade51d8911b0
MashoodAli-Official <MashoodAli3@gmail.com>
2024-10-09 18:56:34 +0500

    Dom

--- Ch.02-ObjectsInJs/08-DOM/dom.js
+++ Ch.02-ObjectsInJs/08-DOM/dom.js
@@ -1,16 +1,6 @@
 // script.js
 const changeTextBtn = document.getElementById('change-text-btn');
 const textToChange = document.getElementById('text-to-change');
-
 changeTextBtn.addEventListener('click', () => {
     textToChange.textContent = 'New text!';
-});
-
-const additionBtn = document.getElementById('addition-btn');
-const additionResult = document.getElementById('addition-result');
-additionBtn.addEventListener('click', () => {
-    const num1 = parseFloat(document.getElementById('num1').value);
-    const num2 = parseFloat(document.getElementById('num2').value);
-    const result = num1 + num2;
-    additionResult.textContent = `Result: ${result}`;
 });
