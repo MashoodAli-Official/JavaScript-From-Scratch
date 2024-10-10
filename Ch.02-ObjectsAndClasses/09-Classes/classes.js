075f0136c8876f52fa75dcd237fdf4eb3b5b5ba5
MashoodAli-Official <MashoodAli3@gmail.com>
2024-10-10 22:28:50 +0500

    class chapter complete

---
+++ Ch.02-ObjectsAndClasses/09-Classes/classes.js
@@ -0,0 +1,15 @@
+class person {
+    constructor(name, age) {
+        console.log("The Person name is : " + name);
+        console.log("The Person age is: " + age);
+    }
+}
+const details = new person("Spider Man", 30);
+
+class study {
+    constructor (program, CGPA) {
+        console.log("The program name is : "+ "Computer Science");
+        console.log("The CGPA is : " + 3.2);
+    }
+}
+const studyDetails = new study("Computer Science", 3.2);
