58d45afff87335235112d1f0c3dfa8b114038ded
MashoodAli-Official <MashoodAli3@gmail.com>
2024-11-03 21:08:24 +0400

    Simple and Anonymous function

---
+++ Ch.06-Functions/01-SimpleFunction/simpleFunction.js
@@ -0,0 +1,9 @@
+function superHeros(){
+    console.log("Captain America");
+    console.log("Iron Man");
+    console.log("Thor");
+    
+    var sum = 10+20;
+    console.log(sum);
+};
+superHeros();
---
+++ Ch.06-Functions/02-AnonymousFunction/app.js
@@ -0,0 +1,10 @@
+'use strict';
+
+var simpleFunction = function() {
+    var a = 1;
+    var b = 2;
+    var c = a + b;
+    console.log(c);
+};
+
+simpleFunction();
