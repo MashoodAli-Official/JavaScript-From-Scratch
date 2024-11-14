3e867b4692c82b9d2bf654d89cc0d3285ecef5d2
MashoodAli-Official <MashoodAli3@gmail.com>
2024-11-14 17:47:14 +0400

    high order function in js

---
+++ Ch.09-HighOrderFunction/hof.js
@@ -0,0 +1,6 @@
+// HIGH ORDER FUNCTION in javascript
+// In javascript, a function which is taking one or more other functions as an argument or returns a function as a result is called a higher-order function as a argument is called High order function.
+
+const num1 = [2,4,6,8,10];
+const doubleNum = num1.map((num) => num * 2);
+console.log(doubleNum)
