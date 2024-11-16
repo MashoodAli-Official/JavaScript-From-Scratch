5162feb329560edfc744253f62e5f266010d05f4
MashoodAli-Official <MashoodAli3@gmail.com>
2024-11-16 22:00:06 +0400

    Immutability topic complete

---
+++ Ch.10-Immutability/immutability.js
@@ -0,0 +1,7 @@
+// Immutability means that an object or data structure cannot be modified after it is created. Instead, when you want to make changes, you create a new object or data structure with the updated values.
+
+let hero = { name: "Iron Man" };
+hero.name = "Hulk"; // The original object is modified
+
+let arr = [1, 2, 3];
+arr.push(4); // The original array is modified
